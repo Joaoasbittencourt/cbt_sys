10ad1520e75c04e7069a2f325897f13cec6bcca0
João Bittencourt <joao@startapp.one>
2020-06-01 00:30:34 -0300

    added a better hitsplash smothing

--- HitSplash.lua
+++ HitSplash.lua
@@ -1,11 +1,10 @@
 
-
 function createHitSplash(value, x, y)
 
 	local _x = x
 	local _y = y
-	local _value = value
 	local _duration = 800
+	local _speed = 200
 
 	return {
 
@@ -14,10 +13,10 @@ function createHitSplash(value, x, y)
 		end,
 
 		update = function(dt)
-			local speed = 15
+			_speed = _speed * 0.95
 			if _duration > 0 then
 				_duration = _duration - dt * 1000
-				_y = _y - dt * speed
+				_y = _y - dt * _speed
 			else
 				_duration = 0
 			end

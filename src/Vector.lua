0fa231b1c28d42a74219c71182aa33bfa52680f3
João Bittencourt <joao@startapp.one>
2020-06-03 21:31:56 -0300

    added scalarDivision to vector

--- src/Vector.lua
+++ src/Vector.lua
@@ -4,32 +4,20 @@ function Vector(x, y)
 	local _x = x or 0
 	local _y = y or 0
 
-	local function getModulus()
-		return math.sqrt(math.pow(_x, 2) + math.pow(_y, 2))
-	end
-
-	local function add(point)
-		return Vector(_x + point.getX(),  _y + point.getY())
-	end
-
-	local function mul(value)
-		return Vector(_x * value, _y * value)
-	end
-
-	local function sub(vec)
-		return Vector(_x - vec.getX(), _y - vec.getY())
-	end
+	local function getModulus() return math.sqrt(math.pow(_x, 2) + math.pow(_y, 2)) end
+	local function mul(scalar) return Vector(_x * scalar, _y * scalar) end
+	local function div(scalar) return Vector(_x / scalar, _y / scalar) end
+	local function add(vec) return Vector(_x + vec.getX(),  _y + vec.getY()) end
+	local function sub(vec) return Vector(_x - vec.getX(), _y - vec.getY()) end
+	local function getUnit() return Vector(_x, _y).div(getModulus()) end
 
 	local function set(point)
 		_x = point.getX()
 		_y = point.getY()
 	end
 
-	local function getUnit()
-		local mod = getModulus()
-		return Vector(_x / mod, _y / mod)
-	end
 
+	self.div = div
 	self.add = add
 	self.mul = mul
 	self.set = set

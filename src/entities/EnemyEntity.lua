957e4e1e467043d1437e45c09fec198c952be951
João Bittencourt <joao@startapp.one>
2020-06-07 19:56:05 -0300

    fixed enemy damage

--- src/entities/EnemyEntity.lua
+++ src/entities/EnemyEntity.lua
@@ -4,6 +4,20 @@ function EnemyEntity()
 	local speed = 120
 	local health = HealthComponent(40, "Zombie")
 	local physics = PhysicsComponent(Vector(400, 400), radius)
+	local attackCooldown = 2000
+
+	local function _handleAttack(dt, playerPos, position)
+		if attackCooldown > 0 then
+			attackCooldown = attackCooldown - dt * 1000
+		end
+
+		if playerPos.distanceTo(position) < player.radius + radius + 5 then
+			if attackCooldown <= 0 then
+				player.health.insertDamage(10 + math.floor(math.random() * 10))
+				attackCooldown = 2000
+			end
+		end
+	end
 
 	local function update(dt, player)
 
@@ -14,11 +28,14 @@ function EnemyEntity()
 			physics.setSpeed(position.directionTo(playerPos).mul(speed))
 		end
 
+		_handleAttack(dt, playerPos, position)
+
 		health.update(dt, position.getX(), position.getY())
 	end
 
 	local function draw()
 		local position = physics.getPosition()
+		love.graphics.setColor(0.8, 0.8, 0.2)
 		love.graphics.circle("fill", position.getX(), position.getY(), radius)
 		health.draw()
 	end
